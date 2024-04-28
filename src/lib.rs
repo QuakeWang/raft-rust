25a4880cbdfcbe11a36ca11b04ef79bcfd4eef25
QuakeWang <1677980708@qq.com>
2024-04-28 17:11:47 +0800

    feat: implement raft log

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod proto;
 mod timer;
 mod peer;
+mod log;
---
+++ src/log.rs
@@ -0,0 +1,73 @@
+pub mod proto {
+    tonic::include_proto!("raft");
+}
+
+#[derive(Debug)]
+pub struct Log {
+    entries: Vec<proto::LogEntry>,
+    start_index: u64,
+}
+
+impl Log {
+    pub fn new(start_index: u64) -> Self {
+        Self {
+            entries: Vec::new(),
+            start_index,
+        }
+    }
+
+    pub fn append_entries(&mut self, entries: Vec<proto::LogEntry>) {
+        self.entries.extend(entries);
+    }
+
+    pub fn get_entries(&self) -> &Vec<proto::LogEntry> {
+        &self.entries
+    }
+
+    pub fn get_entry(&self, index: u64) -> Option<&proto::LogEntry> {
+        self.entries.get(index as usize)
+    }
+
+    pub fn get_start_index(&self) -> u64 {
+        self.start_index
+    }
+
+    pub fn get_last_index(&self) -> u64 {
+        self.start_index + self.entries.len() as u64 - 1
+    }
+
+    pub fn get_last_term(&self) -> u64 {
+        self.entries.last().unwrap().term
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::log::proto::LogEntry;
+
+    #[test]
+    fn test_log() {
+        let mut log = super::Log::new(1);
+        let entry1 = LogEntry {
+            index: 1,
+            term: 1,
+            entry_type: 1,
+            data: "test".as_bytes().to_vec(),
+        };
+        log.append_entries(vec![entry1]);
+
+        let entry2 = LogEntry {
+            index: 2,
+            term: 1,
+            entry_type: 1,
+            data: "test".as_bytes().to_vec(),
+        };
+        log.append_entries(vec![entry2]);
+
+        assert_eq!(log.get_entries().len(), 2);
+        assert_eq!(log.get_start_index(), 1);
+        assert_eq!(log.get_entry(1).unwrap().index, 2);
+        assert_eq!(log.get_last_index(), 2);
+        assert_eq!(log.get_last_term(), 1);
+    }
+}
